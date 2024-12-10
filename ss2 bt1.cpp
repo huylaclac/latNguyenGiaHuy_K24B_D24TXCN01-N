6578d94b6563133e4ef71a69a861055429e81496
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-10 17:02:44 +0700

    Add files via upload

---
+++ ss2 bt1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+	int main() {
+		int a = 3;//khai bao bien  
+		float b = 2.22;
+		char  c = 'A';
+		_Bool e = 1;
+		printf("int: %d\n", a);// kieu so nguyen int  
+		printf("float: %.2f\n", b);//kieu so  thuc float  
+		printf("char: %c\n", c);//kieu ki tu char 
+		printf("_bool: %d", e);// kieu  true false  
+return 0; 
+	} 
