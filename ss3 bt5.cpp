e4694f4e65072cf28915ee1bde51f997cd09a49b
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-11 21:18:44 +0700

    Add files via upload

---
+++ ss3 bt5.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+    int main() {
+        printf("%45s\n", "Danh Sach Sinh Vien");
+        printf("%s\n", "------------------------------------------------------------------------");
+        printf("%s", "| STT | Ho va ten       | tuoi | so dien thoai  | email                 |\n");
+        printf("%s", "| 1   | Nguyen Van A    | 20   | 0904488481     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 2   | Nguyen Van B    | 21   | 0904488482     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 3   | Nguyen Van C    | 18   | 0904488483     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 4   | Nguyen Van D    | 19   | 0904488484     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 5   | Nguyen Van E    | 22   | 0904488485     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 6   | Nguyen Van F    | 21   | 0904488486     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 7   | Nguyen Van G    | 23   | 0904488487     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 8   | Nguyen Van H    | 19   | 0904488488     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 9   | Nguyen Van Z    | 18   | 0904488489     | @rikkeiacademy.com    |\n");
+        printf("%s", "| 10  | Nguyen Van F    | 21   | 0904488486     | @rikkeiacademy.com    |\n");
+        
+        return 0;
+    }
