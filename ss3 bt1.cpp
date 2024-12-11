4417d507694ba138d0368e2ddd448477ac0f4c3f
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-11 20:59:38 +0700

    Add files via upload

---
+++ ss3 bt1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+    int main() {
+        char ten[10];
+        printf("nhap ten cua ban: ");
+        scanf("%s", &ten);
+        printf("chao ban %s!\n", ten);
+        return 0;
+    }
