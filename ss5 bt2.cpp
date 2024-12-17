1ca68531e00694bfc41cf31574288676349a911b
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-17 09:34:13 +0700

    Add files via upload

---
+++ ss5 bt2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main() {
+       int sogoc=50, answer;
+       do{
+       printf("nhap vap dap an mot so nguyen: ");
+       scanf("%d", &answer);
+       } while(answer != sogoc);
+       printf("chuan r ");
+       
+    return 0;
+}
