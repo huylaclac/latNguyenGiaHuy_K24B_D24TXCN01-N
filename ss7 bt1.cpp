f4fcfa5cf4a374c135f6f55fc350eb2a70ca3f5a
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-18 20:29:59 +0700

    Add files via upload

---
+++ ss7 bt1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int N,tong ;
+    printf("nhap vao N: ");
+    scanf("%d", &N);
+    int a[5];
+    for(int i=0; i<N; i++){
+        printf("%d ", a[i]);
+    }
+printf("\ntong cua cac phan tu la: %d", tong);
+printf("\ndo dai cua mang la: %d", N);
+    return 0;
+}
