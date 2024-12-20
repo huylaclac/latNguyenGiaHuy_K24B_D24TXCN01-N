b13909cf74bd24865043c9ab4242ba95bac7b958
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-20 08:01:13 +0700

    Add files via upload

--- ss8 bt5.cpp
+++ ss8 bt5.cpp
@@ -12,20 +12,15 @@
     }
 			for (int i=0; i<N;i++){
 				for(int j=0;j<N;j++){
-				if(i==0||j==0||i==N-1||j==N-1){
-					tong=tong+a[i][j];
+				printf("%d ", a[i][j]);							
+					}	
+					printf("\n");											
 				}
-			}
-		}
+				 printf("Cac phan tu tren duong cheo chinh la");
 			for (int i=0; i<N;i++){
-				for(int j=0;j<N;j++){
-				printf("%d ", a[i][j]);
-				
-				
-					}
-					
-						printf("\n");	
+				printf("%d", a[i][i]);
+					tong=tong+a[i][i];
 				}
-				printf("tong cac phan tu bien la: %d", tong);
+				printf("tong cac phan tu dg cheo chinh: %d", tong);
 	return 0;
 }
