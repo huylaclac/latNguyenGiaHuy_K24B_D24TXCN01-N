e7d03d1411479f739cc17074e18df582a7207311
huylaclac <nguyengialac29lequydon@gmail.com>
2024-12-29 09:07:21 +0700

    Add files via upload

---
+++ ss12 bt2 lac.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ void inmang(int a[], int N){
+ 	for(int i=0;i<N;i++){
+ 		printf("%d ", a[i]);
+	 }
+ }
+	int main() {
+		int N;
+		printf("nhap vao do dai cua mang: ");
+		scanf("%d", &N);
+		int a[N];
+		printf("arr[%d]: ", i);
+		}
+		inmang(a, N);
+		
+	}
